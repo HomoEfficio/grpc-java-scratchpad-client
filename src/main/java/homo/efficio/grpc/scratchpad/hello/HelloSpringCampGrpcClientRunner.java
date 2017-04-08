59a62f840d0f494ff91e35f45336f34ae1b12fc3
Homo Efficio <homo.efficio@gmail.com>
2017-04-08 16:40:11 +0900

    ClientRunner에 clientStreamingHello() 추가
    asyncStub을 통해 호출하므로 Thread.sleep()으로 기다려줘야 결과 받을 수 있음

--- src/main/java/homo/efficio/grpc/scratchpad/hello/HelloSpringCampGrpcClientRunner.java
+++ src/main/java/homo/efficio/grpc/scratchpad/hello/HelloSpringCampGrpcClientRunner.java
@@ -1,5 +1,7 @@
 package homo.efficio.grpc.scratchpad.hello;
 
+import java.util.Arrays;
+
 /**
  * @author homo.efficio@gmail.com
  *         created on 2017. 4. 6.
@@ -18,7 +20,13 @@ public class HelloSpringCampGrpcClientRunner {
                                               clientStub.getAsyncStub(),
                                               clientStub.getFutureStub());
 
-        grpcClient.sendBlockingUnaryMessage("homo.efficio");
+//        grpcClient.sendBlockingUnaryMessage("homo.efficio");
+
+        grpcClient.sendAsyncClientStreamingMessage(
+                Arrays.asList("클라이언트 스트리밍", "gRPC", "스프링캠프 2017")
+        );
+
+        Thread.sleep(2000);
 
         clientStub.shutdown();
     }
